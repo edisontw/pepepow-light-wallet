f02253699091b4208c0084c91316022ea58d62b1
Edison Huang <107197406+edisontw@users.noreply.github.com>
2026-07-01 21:55:51 +0800

    Disable non-Light API routes in public wallet

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -3,20 +3,17 @@ import "./styles/ui.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import WalletHome from "./pages/WalletHome";
 import History from "./pages/History";
-import Mini from "./pages/Mini";
-import Pay from "./pages/Pay";
-import Claim from "./pages/Claim";
 
 export default function App() {
   return (
     <BrowserRouter basename="/wallet">
       <Routes>
         <Route path="/" element={<WalletHome />} />
-        <Route path="/send" element={<Navigate to="/" replace />} />
         <Route path="/history" element={<History />} />
-        <Route path="/mini" element={<Mini />} />
-        <Route path="/pay/:token" element={<Pay />} />
-        <Route path="/claim" element={<Claim />} />
+        <Route path="/send" element={<Navigate to="/" replace />} />
+        <Route path="/mini" element={<Navigate to="/" replace />} />
+        <Route path="/pay/:token" element={<Navigate to="/" replace />} />
+        <Route path="/claim" element={<Navigate to="/" replace />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
